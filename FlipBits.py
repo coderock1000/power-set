c3f4ca1dc1989ee557a65fa2f02deddf09a9f527
coderock100 <vd.ssaathvik@gmail.com>
2024-11-23 21:59:47 +0530

    flips needed

---
+++ FlipBits.py
@@ -0,0 +1,17 @@
+def totalflips(num1, num2):
+    flips = 0
+    while(num1 > 0 or num2 > 0):
+        t1 = num1 & 1
+        t2 = num2 & 1
+        if t1 != t2:
+            flips += 1
+
+        num1 >>= 1
+        num2 >>= 1
+
+    return flips
+
+num1 =int(input('enter the first number: '))
+num2 =int(input('enter the second number: '))
+
+print('number of flips needed: ', totalflips(num1,num2))
