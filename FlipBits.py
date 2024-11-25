7064e35d763f5a7ef3ab53ea03ee9fb48c0a8082
coderock100 <vd.ssaathvik@gmail.com>
2024-11-25 16:42:57 +0530

    flip bits

--- FlipBits.py
+++ FlipBits.py
@@ -14,4 +14,11 @@ def totalflips(num1, num2):
 num1 =int(input('enter the first number: '))
 num2 =int(input('enter the second number: '))
 
+print('\n')
+
+print('(first number) = ',num1)
+print('(second number) = ',num2)
+
+print('\n')
+
 print('number of flips needed: ', totalflips(num1,num2))
